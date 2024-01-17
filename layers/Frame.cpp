e1ae4064a0f3734f8041f5cd8ae25289b9923d07
andre <awermersbach0361@floridapoly.edu>
2024-01-17 12:29:47 -0500

    added frame class

---
+++ layers/Frame.cpp
@@ -0,0 +1,12 @@
+//
+// Created by andre on 1/17/2024.
+//
+
+#include "Frame.h"
+
+Frame::Frame(uint32_t _msg_len, uint8_t* _msg){
+
+}
+Frame::uint32_t calculateCRC(uint8_t* _msg){
+
+}
---
+++ layers/Frame.h
@@ -0,0 +1,25 @@
+//
+// Created by Andrew on 1/17/2024.
+//
+
+#ifndef NETWORKTESTER_FRAME_H
+#define NETWORKTESTER_FRAME_H
+
+#include <stdint.h>
+
+
+
+class Frame {
+public:
+    Frame(uint32_t _msg_len, uint8_t* _msg);
+    uint32_t calculateCRC(uint8_t* _msg);
+
+private:
+    uint32_t msg_len;
+    uint8_t* msg;
+    uint32_t crc;
+};
+Frame fromBytestream();
+Frame toBytestream();
+
+#endif //NETWORKTESTER_FRAME_H
