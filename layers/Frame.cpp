e6e09f1d542bd395dec2ed81ba3d0de6ecd73520
andre <awermersbach0361@floridapoly.edu>
2024-01-17 15:53:04 -0500

    finished calculate_crc and tester

--- layers/Frame.cpp
+++ layers/Frame.cpp
@@ -4,7 +4,6 @@
 
 #include "Frame.h"
 uint32_t crc_polynomial = 0xEDB88320;
-uint32_t crc_verify = 0x38FB2284;
 Frame::Frame(uint32_t _msg_len, uint8_t* _msg){
     msg = _msg;
     msg_len = _msg_len;
@@ -27,16 +26,6 @@ void Frame::calculate_crc() {
 
     // The final CRC value is the one's complement of the calculated CRC
     crc = ~crc;
-
-    // Print the calculated CRC for verification
-    std::cout << "Calculated CRC: 0x" << std::hex << crc << std::endl;
-
-    // Verify against the specified CRC verification value (0x38FB2284)
-    if (crc == crc_verify) {
-        std::cout << "CRC verification passed." << std::endl;
-    } else {
-        std::cerr << "CRC verification failed." << std::endl;
-    }
 }
 uint32_t Frame::get_crc(){
     return crc;
