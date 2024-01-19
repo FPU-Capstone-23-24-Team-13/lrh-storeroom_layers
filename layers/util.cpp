a045f524d63ab23ae01e26bf8bb600187022ca8e
The Randalorian <dwjgame11@gmail.com>
2024-01-18 19:28:00 -0500

    Add uint16 helpers

--- layers/util.cpp
+++ layers/util.cpp
@@ -7,7 +7,7 @@
 NetworkInterface* network_interfaces = nullptr;
 int network_interface_count = 0;
 
-uint32_t get_uint32_from_pointer(uint8_t* pointer){
+uint32_t get_uint32_from_pointer(const uint8_t* pointer){
     uint32_t val = 0x00000000;
     val |= *(pointer + 0) << 0;
     val |= *(pointer + 1) << 8;
@@ -21,4 +21,16 @@ void put_uint32_to_pointer(uint8_t* pointer, uint32_t val){
     *(pointer + 1) = (val >> 8) & 0x000000FF;
     *(pointer + 2) = (val >> 16) & 0x000000FF;
     *(pointer + 3) = (val >> 24) & 0x000000FF;
+}
+
+uint16_t get_uint16_from_pointer(const uint8_t* pointer){
+    uint16_t val = 0x00000000;
+    val |= *(pointer + 0) << 0;
+    val |= *(pointer + 1) << 8;
+    return val;
+}
+
+void put_uint16_to_pointer(uint8_t* pointer, uint16_t val){
+    *(pointer + 0) = (val >> 0) & 0x00FF;
+    *(pointer + 1) = (val >> 8) & 0x00FF;
 }
