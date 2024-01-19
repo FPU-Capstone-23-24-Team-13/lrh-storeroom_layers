1e8078ef955e196894812f66f492e90fdd1ea19f
The Randalorian <dwjgame11@gmail.com>
2024-01-18 19:29:15 -0500

    Add uint16 helpers to header

--- layers/util.h
+++ layers/util.h
@@ -11,6 +11,8 @@
 
 uint32_t get_uint32_from_pointer(uint8_t* pointer);
 void put_uint32_to_pointer(uint8_t* pointer, uint32_t val);
+uint16_t get_uint16_from_pointer(uint8_t* pointer);
+void put_uint16_to_pointer(uint8_t* pointer, uint16_t val);
 
 extern NetworkInterface* network_interfaces;
 extern int network_interface_count;
