9cdae2eb4a48a42d9683caa6e0c01c9b63d90d8a
The Randalorian <dwjgame11@gmail.com>
2024-02-04 18:55:06 -0500

    Add listener example

---
+++ example/listener/listener.cpp
@@ -0,0 +1,28 @@
+//
+// Created by rando on 2/4/24.
+//
+
+#include <core/NetworkInterface.h>
+#include <core/Packet.h>
+#include <core/util.h>
+#include <rlc/TtyNetworkInterface.h>
+#include <core/Frame.h>
+
+int main() {
+    lrhnet::NetworkInterface* ni[] = {
+            new lrhnet::TtyNetworkInterface(0, const_cast<char*>("/dev/ttyACM0"), 921600)
+    };
+    lrhnet::network_interfaces = ni;
+    lrhnet::network_interface_count = 1;
+
+    for (int i = 0; i < 256; ++i){
+        lrhnet::port_callbacks[i] = lrhnet::debug_port_callback;
+    }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (true){
+        lrhnet::poll();
+    }
+#pragma clang diagnostic pop
+}
