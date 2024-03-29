6f67f9a58bd7f47d6d4302a25890f24d9c951d84
The Randalorian <dwjgame11@gmail.com>
2024-03-29 18:00:52 -0400

    Setup Time Protocol

--- example/calibrator/calibrator.cpp
+++ example/calibrator/calibrator.cpp
@@ -91,7 +91,37 @@ void callback_calibrate_ready([[maybe_unused]] uint64_t source, [[maybe_unused]]
     cv.notify_all();
 }
 
+void callback_time(uint64_t source, uint8_t port, uint8_t *message, uint32_t length){
+    printf("get_time!");
+    lrhnet::debug_port_callback(source, port, message, length);
+    long long int mn;
+    while (length > 0 && (message[0] == '\n' || message[0] == '\r' || message[0] == '\t' || message[0] == ' ')){
+        message += sizeof(message[0]);
+        length--;
+    }
+    sscanf(reinterpret_cast<const char *>(message), "get_time(%lld)", &mn);
+
+    time_t t = time(nullptr);
+    struct tm tm = *localtime(&t);
+
+    int16_t year = tm.tm_year + 1900;
+    int16_t month = tm.tm_mon + 1;
+    int16_t day = tm.tm_mday;
+    int16_t dotw = tm.tm_wday;
+    int16_t hour = tm.tm_hour;
+    int16_t min = tm.tm_min;
+    int16_t sec = tm.tm_sec;
+
+    int buf_size = 1024;
+    uint8_t response_message[buf_size];
+    uint32_t response_length = snprintf(reinterpret_cast<char *>(response_message), buf_size, "\nset_time(%lld, %hd, %hd, %hd, %hd, %hd, %hd, %hd)\n", mn, year, month, day, dotw, hour, min, sec);
+
+    printf("response->%s\n", reinterpret_cast<const char *>(response_message));
+    lrhnet::send_message(source, TIME_PORT, response_message, response_length);
+}
+
 void callback_calibrate(uint64_t source, uint8_t port, uint8_t *message, uint32_t length) {
+    //lrhnet::debug_port_callback(source, port, message, length);
     char *cleaned_message = remove_whitespace(reinterpret_cast<char *>(message));
     char *command = get_command(cleaned_message);
 
@@ -151,14 +181,17 @@ int main() {
     lrhnet::device_id = 0x0000000000000000;
 
     lrhnet::NetworkInterface* ni[] = {
-            new lrhnet::TtyNetworkInterface(0, const_cast<char*>("/dev/serial/by-id/usb-Florida_Polytechnic_LRHNET_Interface_E661385283312D25-if00"), 921600)
+            new lrhnet::TtyNetworkInterface(0, const_cast<char*>("/dev/serial/by-id/usb-Florida_Polytechnic_LRHNET_Interface_E661640843945622-if00"), 921600)
     };
     lrhnet::network_interfaces = ni;
     lrhnet::network_interface_count = 1;
 
+    //for (int i = 0; i < 256; ++i) lrhnet::port_callbacks[i] = lrhnet::debug_port_callback;
+
     lrhnet::port_callbacks[CALIBRATE_PORT] = callback_calibrate;
     lrhnet::port_callbacks[PRESENCE_PORT] = callback_presence;
     lrhnet::port_callbacks[NAME_PORT] = callback_name;
+    lrhnet::port_callbacks[TIME_PORT] = callback_time;
 
     show_presence_messages = true;
 
@@ -258,6 +291,7 @@ int main() {
     ticket = rand();
     length = snprintf(reinterpret_cast<char *>(message), buf_size, "\ncalibrate_finalize(%d, %d, %d)\n", mn, session, ticket);
     lrhnet::send_message(target_device, CALIBRATE_PORT, message, length);
+    sleep(10);
 
     running = false;
     t.join();
--- example/listener/listener.cpp
+++ example/listener/listener.cpp
@@ -8,11 +8,42 @@
 #include <rlc/TtyNetworkInterface.h>
 #include <core/Frame.h>
 
+#define TIME_PORT 13
+
+void callback_time(uint64_t source, uint8_t port, uint8_t *message, uint32_t length){
+    printf("get_time!");
+    lrhnet::debug_port_callback(source, port, message, length);
+    long long int mn;
+    while (length > 0 && (message[0] == '\n' || message[0] == '\r' || message[0] == '\t' || message[0] == ' ')){
+        message += sizeof(message[0]);
+        length--;
+    }
+    sscanf(reinterpret_cast<const char *>(message), "get_time(%lld)", &mn);
+
+    time_t t = time(nullptr);
+    struct tm tm = *localtime(&t);
+
+    int16_t year = tm.tm_year + 1900;
+    int16_t month = tm.tm_mon + 1;
+    int16_t day = tm.tm_mday;
+    int16_t dotw = tm.tm_wday;
+    int16_t hour = tm.tm_hour;
+    int16_t min = tm.tm_min;
+    int16_t sec = tm.tm_sec;
+
+    int buf_size = 1024;
+    uint8_t response_message[buf_size];
+    uint32_t response_length = snprintf(reinterpret_cast<char *>(response_message), buf_size, "\nset_time(%lld, %hd, %hd, %hd, %hd, %hd, %hd, %hd)\n", mn, year, month, day, dotw, hour, min, sec);
+
+    printf("response->%s\n", reinterpret_cast<const char *>(response_message));
+    lrhnet::send_message(source, TIME_PORT, response_message, response_length);
+}
+
 int main() {
     lrhnet::device_id = 0x0000000000000000;
 
     lrhnet::NetworkInterface* ni[] = {
-            new lrhnet::TtyNetworkInterface(0, const_cast<char*>("/dev/ttyACM0"), 115200)
+            new lrhnet::TtyNetworkInterface(0, const_cast<char*>("/dev/serial/by-id/usb-Florida_Polytechnic_LRHNET_Interface_E661640843945622-if00"), 115200)
     };
     lrhnet::network_interfaces = ni;
     lrhnet::network_interface_count = 1;
@@ -20,6 +51,7 @@ int main() {
     for (int i = 0; i < 256; ++i){
         lrhnet::port_callbacks[i] = lrhnet::debug_port_callback;
     }
+    lrhnet::port_callbacks[TIME_PORT] = callback_time;
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
